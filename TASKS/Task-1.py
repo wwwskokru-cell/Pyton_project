088fe561d2308f9f1927745bd74334f8aa30a5e7
wwwskokru-cell <www.skok.ru@gmail.com>
2025-09-07 14:10:03 +0300

    update_trom_07.09.2025

--- TASKS/Task-1.py
+++ TASKS/Task-1.py
@@ -1,32 +1,113 @@
 '''
 Задача: нужно вывести победные комбинации в покере по цифрам 1-13
 '''
-entering_numbers = input("Введите 5 чисел через запятую от 1 до 13: ").lower().strip()
-entering_numbers_list = entering_numbers.split(",")
-entering_numbers_int = []
-for i in entering_numbers_list:
-    i = int(i)
-    entering_numbers_int.append(i)
-answer = {
-    "Пары" : 0,
-    "Сет" : 0,
-    "Карэ" : 0
-}
-_ = 1
-number_of_cards ={}
-while _ < 14:
-    number_of_cards [_]=entering_numbers_int.count(_)
-    if number_of_cards [_] == 0:
-        del number_of_cards[_]
-    elif number_of_cards [_] == 2:
-        answer["Пары"] += 1
-    elif number_of_cards [_] == 3:
-        answer["Сет"] += 1
-    elif number_of_cards [_] == 4:
-        answer["Карэ"] += 1
-    _+=1
+from collections import Counter
 
-print(answer)
 
+
+def get_user_input ():
+    is_input_valid = True
+    while is_input_valid:
+        entering_numbers = input("Введите 5 чисел через запятую от 1 до 13: ").lower().strip()
+        entering_numbers_list = entering_numbers.split(",")
+        if len(entering_numbers_list) != 5:
+            print("Введите 5 целых чисел")
+            continue
+
+        entering_numbers_int = []
+        try:
+            for i in entering_numbers_list:
+                i = int(i)
+                entering_numbers_int.append(i)
+        except ValueError:
+            print("Вводите пожалучта только целые числа")
+            continue
+
+        if entering_numbers_int[0] == entering_numbers_int[1] == entering_numbers_int[2] == entering_numbers_int[3] == entering_numbers_int[4]:
+            print("К сожалению в колоде только по 4 одинаковых карты, вы ввели 5, это недопустимо")
+            continue
+
+        index = 0
+        for num in entering_numbers_int:
+            if 0 < num < 14:
+                index += 1
+                if index == 5:
+                    is_input_valid = False
+            else:
+                print("Вводите только числа от 1 до 13")
+                continue
+
+    return entering_numbers_int 
+
+
+def counting_pairs_set_card (entering_numbers_int ):
+    answer = {
+        "Пары" : 0,
+        "Сет" : 0,
+        "Каре" : 0
+    }
+    num_dict = Counter(entering_numbers_int)
+    for i in num_dict:
+        if num_dict[i] == 2:
+            answer["Пары"] += 1
+        elif num_dict[i] == 3:
+            answer["Сет"] += 1
+        elif num_dict[i] == 4:
+            answer["Каре"] += 1
+    return answer
+
+
+def counting_street (entering_numbers_int):
+    number_int_sort = set(entering_numbers_int)
+    number_int_sort = sorted(number_int_sort)
+    checking_street = True
+    if len(number_int_sort) == 5:
+        for i in range(4):
+            if number_int_sort[i+1]- number_int_sort[i] == 1:
+                checking_street = True
+            else:
+                checking_street = False
+                break
+    else:
+        checking_street = False               
+    return checking_street
+
+
+def answer_final (answer, checking_street):
+    answer_final_dict = {
+        "Каре": 0,
+        "Фул Хаус" : 0,
+        "Стрит" : 0,
+        "Сет" : 0,
+        "2 Пары" : 0,
+        "Пара" : 0
+        }
     
+    if answer["Каре"] == 1:
+        answer_final_dict["Каре"] += 1 
+    elif answer["Сет"] == 1 and answer["Пары"] == 1:
+        answer_final_dict["Фул Хаус"] += 1 
+        answer["Сет"] -=1
+        answer["Пары"] -=1 
+    elif checking_street == True:
+        answer_final_dict["Стрит"] += 1
+    elif answer["Сет"] == 1:
+        answer_final_dict["Сет"] += 1
+    elif answer["Пары"] == 2:
+        answer_final_dict["2 Пары"] += 1
+        answer["Пары"] -= 2
+    elif answer["Пары"] == 1:
+        answer_final_dict["Пара"] += 1
+    
+    for i in list(answer_final_dict.keys()):
+        if answer_final_dict[i] == 0:
+            del answer_final_dict[i]
 
+    return answer_final_dict
+
+
+if __name__ == "__main__":
+    entering_numbers_int = get_user_input ()
+    checking_street = counting_street(entering_numbers_int)
+    answer = counting_pairs_set_card (entering_numbers_int)
+    print(answer_final (answer, checking_street))
---
+++ TASKS/draft.py
@@ -0,0 +1,53 @@
+'''
+Задача: нужно вывести победные комбинации в покере по цифрам 1-13
+'''
+from collections import Counter
+
+def get_user_input ():
+    is_input_valid = True
+    while is_input_valid:
+        entering_numbers = input("Введите 5 чисел через запятую от 1 до 13: ").lower().strip()
+        entering_numbers_list = entering_numbers.split(",")
+        if len(entering_numbers_list) != 5:
+            print("Введите 5 целых чисел")
+            continue
+
+        entering_numbers_int = []
+        try:
+            for i in entering_numbers_list:
+                i = int(i)
+                entering_numbers_int.append(i)
+        except ValueError:
+            print("Вводите пожалучта только целые числа")
+            continue
+               
+        index = 0
+        for num in entering_numbers_int:
+            if 0 < num < 14:
+                index += 1
+                if index == 5:
+                    is_input_valid = False
+            else:
+                print("Вводите только числа от 1 до 13")
+                continue
+    return entering_numbers_int 
+
+def counting_pairs_set_card (entering_numbers_int ):
+    answer = {
+        "Пары" : 0,
+        "Сет" : 0,
+        "Карэ" : 0
+    }
+    num_dict = Counter(entering_numbers_int)
+
+    for i in num_dict:
+        if num_dict[i] == 2:
+            answer["Пары"] += 1
+        elif num_dict[i] == 3:
+            answer["Сет"] += 1
+        elif num_dict[i] == 4:
+            answer["Каре"] += 1
+    return answer
+
+print (counting_pairs_set_card(get_user_input ()))
+ 
